0fd44c986ec6b7f8b22b32b69d90360f56841797
Rob Knight <12484127+rknightion@users.noreply.github.com>
2026-02-14 12:16:12 +0000

    chore: fix ci

--- e2e/helpers/extension.ts
+++ e2e/helpers/extension.ts
@@ -24,6 +24,7 @@ export async function launchBrowserWithExtension(): Promise<Browser> {
       '--no-first-run',
       '--disable-default-apps',
       '--disable-gpu',
+      ...(process.env.CI ? ['--no-sandbox'] : []),
     ],
   });
 }
